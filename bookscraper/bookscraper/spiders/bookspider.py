6ea58a2c5d4b7baff1bcdf41ad5a5177dafcbf3f
I-Mougios <i.mougios.tech@gmail.com>
2025-06-25 15:26:21 +0300

    feat(bookscraper): implement 'bookspider' and the corresponding 'BookItem'

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,4 +1,7 @@
+from itertools import islice
+
 import scrapy
+from bookscraper import BookItem
 from icecream import ic
 
 
@@ -9,14 +12,40 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse(self, response):
         books = response.css("article.product_pod")
-        for book in books:
-            yield {
-                "title": book.css("h3 a::attr(title)").get(),
-                "price": book.css("div.product_price p.price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"],
-            }
+        for book in islice(books, 1):
+            book_url = response.urljoin(book.css("a::attr(href)").get())
+            yield response.follow(book_url, callback=self.parse_book)
 
         next_page = response.css("li.next a::attr(href)").get()
-        if next_page is not None:
-            full_url = ic(response.urljoin(next_page))
-            yield response.follow(full_url, callback=self.parse)
+
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_book(self, response):
+        table_rows = response.css("table tr")
+
+        url = response.url
+        title = response.css("h1::text").get()
+        category = response.xpath("/html/body/div/div/ul/li[3]/a/text()").get()
+        stars = response.css("p.star-rating::attr(class)").get()
+        upc = table_rows[0].css("td::text").get()
+        product_type = table_rows[1].css("td::text").get()
+        price_excl_vat = table_rows[2].css("td::text").get()
+        price_incl_vat = table_rows[3].css("td::text").get()
+        availability = table_rows[5].css("td::text").get()
+        n_reviews = table_rows[6].css("td::text").get()
+        book_item = BookItem(
+            url=url,
+            title=title,
+            upc=upc,
+            category=category,
+            stars=stars,
+            product_type=product_type,
+            price_excl_vat=price_excl_vat,
+            price_incl_vat=price_incl_vat,
+            availability=availability,
+            n_reviews=n_reviews,
+        )
+        ic(book_item)
+        yield book_item
